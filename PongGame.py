c93b692dbb9428d2e2b0988f65b4ed26159690c5
De'Unte Chatman <117577771+DeUnte@users.noreply.github.com>
2022-11-29 01:01:38 -0500

    Update PongGame.py

--- PongGame.py
+++ PongGame.py
@@ -1,7 +1,5 @@
-# Pong Game
 
 import turtle
-import winsound
 
 wn = turtle.Screen()
 wn.title("Pong Game")
@@ -9,12 +7,10 @@ wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
-#Score
 score_a = 0
 score_b = 0
 
 
-# Paddle A
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
 paddle_a.shape("square")
@@ -23,7 +19,6 @@ paddle_a.shapesize(5, 1)
 paddle_a.penup()
 paddle_a.goto(-350, 0)
 
-# Paddle B
 paddle_b = turtle.Turtle()
 paddle_b.speed(0)
 paddle_b.shape("square")
@@ -32,7 +27,6 @@ paddle_b.shapesize(stretch_wid=5, stretch_len=1)
 paddle_b.penup()
 paddle_b.goto(350, 0)
 
-# Ball
 ball = turtle.Turtle()
 ball.speed()
 ball.shape("circle")
@@ -42,7 +36,6 @@ ball.goto(0, 0)
 ball.dx = -.3
 ball.dy = -.3
 
-# Pen
 pen = turtle.Turtle()
 pen.speed(0)
 pen.color("white")
@@ -51,7 +44,6 @@ pen.hideturtle()
 pen.goto(0, 260)
 pen.write("Player A: 0 Player B: 0", align= "center", font=("Courier", 24, "normal"))
 
-# Functions
 def paddle_a_up():
     y = paddle_a.ycor()
     y += 20
@@ -72,22 +64,18 @@ def paddle_b_down():
     y -= 20
     paddle_b.sety(y)
 
-# Keyboard binding
 wn.listen()
 wn.onkeypress(paddle_a_up, "w")
 wn.onkeypress(paddle_a_down, "s")
 wn.onkeypress(paddle_b_up, "Up")
 wn.onkeypress(paddle_b_down, "Down")
 
-# Main game loop
 while True:
     wn.update()
 
-    # Move the Ball
     ball.setx(ball.xcor() + ball.dx)
     ball.sety(ball.ycor() + ball.dy)
 
-    # Border Checking
     if ball.ycor() > 290:
         ball.sety(290)
         ball.dy *= -1
@@ -111,7 +99,6 @@ while True:
         pen.write("Player A: {} Player B: {}".format(score_a, score_b), align= "center", font=("Courier", 24, "normal"))
 
 
-    # Paddle and ball collisions
     if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40):
         ball.setx(340)
         ball.dx *= -1
